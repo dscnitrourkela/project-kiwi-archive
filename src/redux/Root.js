5ab7d25fa653e6c1cd324bfd85bfd63617f2823a
ankitkumarsamota121 <ankitkumarsamota121@gmail.com>
2020-12-01 22:30:25 +0530

    Project Setup

---
+++ src/redux/Root.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { createStore, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
+import { composeWithDevTools } from 'redux-devtools-extension'
+
+import reducers from './reducers'
+
+const initialState = {}
+const middlewares = [thunk]
+
+const store = createStore(
+  reducers,
+  initialState,
+  composeWithDevTools(applyMiddleware(...middlewares))
+)
+
+const Root = ({ initialState = {}, children }) => {
+  return <Provider store={store}>{children}</Provider>
+}
+
+export default Root
