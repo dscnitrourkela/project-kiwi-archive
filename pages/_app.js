06e26112bc2ed4a2b21d182c5e1c406ef8a2c408
ankitkumarsamota121 <ankitkumarsamota121@gmail.com>
2020-12-01 22:37:11 +0530

    _app.js setup

--- pages/_app.js
+++ pages/_app.js
@@ -6,14 +6,18 @@ import theme from '@hackclub/theme'
 import { ThemeProvider } from 'theme-ui'
 import ColorSwitcher from '../components/color-switcher'
 
+import Root from '../src/redux/Root'
+
 export default class App extends NextApp {
   render() {
     const { Component, pageProps } = this.props
     return (
-      <ThemeProvider theme={theme}>
-        <ColorSwitcher />
-        <Component {...pageProps} />
-      </ThemeProvider>
+      <Root>
+        <ThemeProvider theme={theme}>
+          <ColorSwitcher />
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </Root>
     )
   }
 }
