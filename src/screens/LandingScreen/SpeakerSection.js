997b225b5588c4c2adb7266b334d814ac00f3fcd
CIPHERTron <pritish.samal918@gmail.com>
2021-02-19 21:49:31 +0530

    Setup Gaurav Sen Profile Sample

--- src/screens/LandingScreen/SpeakerSection.js
+++ src/screens/LandingScreen/SpeakerSection.js
@@ -41,7 +41,7 @@ const SpeakerSection = () => {
         <Heading
           as="h2"
           variant="title"
-          sx={{ textAlign: 'center', mb: [5, 6], mt: [5, 5] }}
+          sx={{ textAlign: 'center', mb: [5, 5], mt: [5, 5] }}
         >
           <Text
             as="span"
@@ -65,7 +65,7 @@ const SpeakerSection = () => {
             alignItems: 'center',
             justifyContent: 'center',
             margin: '0 auto',
-            mb: [5, 6]
+            mb: [5, 5]
           }}
         >
           <Container
@@ -305,6 +305,133 @@ const SpeakerSection = () => {
                     >
                       Register
                   </Button> */}
+                </Box>
+              </Box>
+            </Box>
+          </Container>
+        </Grid>
+
+        <Grid
+          columns={[1, 1]}
+          gap={5}
+          sx={{
+            alignItems: 'center',
+            justifyContent: 'center',
+            margin: '0 auto',
+            mb: [5, 6]
+          }}
+        >
+          <Container
+            sx={{
+              maxWidth: ['400px', '600px'],
+              cursor: 'pointer',
+              margin: '0 auto',
+              width: 'fit-content',
+              padding: '0',
+              borderRadius: '100px',
+              transition: 'transform .5s ease-in-out',
+              ':hover': {
+                transform: 'scale(1.1)'
+              }
+            }}
+          >
+            <Box
+              sx={{
+                display: 'flex',
+                flexDirection: ['column', 'row'],
+                boxShadow: '2px 2px 10px #000',
+                borderRadius: '10px'
+              }}
+            >
+              <Image
+                sx={{
+                  objectFit: 'cover',
+                  borderRadius: ['10px 10px 0 0', '10px 0 0 10px'],
+                  minWidth: '40%'
+                }}
+                src="/images/Speakers/gauravsen.jfif"
+              />
+              <Box
+                sx={{
+                  color: '#8492A6',
+                  background: '#252429',
+                  minWidth: ['200px', '300px'],
+                  paddingLeft: '1rem',
+                  paddingRight: '10px',
+                  borderRadius: ['0 0 10px 10px', '0 10px 10px 0']
+                }}
+              >
+                <Heading
+                  sx={{
+                    textAlign: 'left',
+                    pt: ['0.7rem', '1.5rem'],
+                    pb: [1, 1],
+                    fontSize: [2, 3],
+                    color: '#fff'
+                  }}
+                >
+                  Gaurav Sen
+                </Heading>
+                <Heading
+                  sx={{
+                    textAlign: 'left',
+                    fontSize: '16px',
+                    pb: [1, 3],
+                    fontWeight: '400',
+                    color: '#fff'
+                  }}
+                >
+                  Student Program Manager, Github Education
+                </Heading>
+                <Text
+                  sx={{
+                    textAlign: 'left',
+                    fontSize: '14px',
+                    pt: [1, 2],
+                    pb: [1, 3]
+                  }}
+                >
+                  Juan is a Program Manager at the GitHub Education Team, where
+                  he supports student leaders to build and grow local technical
+                  communities in their schools.
+                </Text>
+                <Box
+                  sx={{
+                    display: 'flex',
+                    flexDirection: 'row',
+                    justifyContent: 'start',
+                    paddingBottom: ['0.6rem', '1rem'],
+                    paddingTop: '5px'
+                  }}
+                >
+                  <Service
+                    href="https://twitter.com/juanpflores_"
+                    size={24}
+                    sx={{
+                      justifySelf: 'center',
+                      color: '#8492A6',
+                      ':hover': {
+                        color: '#EC3750'
+                      }
+                    }}
+                    icon="twitter"
+                    title="Juan on Twitter"
+                  />
+
+                  <Service
+                    href="https://github.com/juanpflores"
+                    size={24}
+                    sx={{
+                      justifySelf: 'center',
+                      pl: '1rem',
+                      color: '#8492A6',
+                      ':hover': {
+                        color: '#EC3750'
+                      }
+                    }}
+                    icon="github"
+                    title="Juan on GitHub"
+                  />
                 </Box>
               </Box>
             </Box>
