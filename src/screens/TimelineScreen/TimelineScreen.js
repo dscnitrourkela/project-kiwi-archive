c0df1fcd80f7e1b821afc745e9cb80f21867ecfe
HarishTeens <arishh2@gmail.com>
2021-03-07 00:19:13 +0530

    popoulate timeline

--- src/screens/TimelineScreen/TimelineScreen.js
+++ src/screens/TimelineScreen/TimelineScreen.js
@@ -1,5 +1,8 @@
 import { Box, Button, Container, Grid, Heading, Text, Link } from 'theme-ui';
 import theme from '../../lib/theme';
+import timelineData from './data.json';
+
+
 
 const Gallery = () => {
   return (
@@ -31,248 +34,27 @@ const Gallery = () => {
         <table>
           <thead>
             <tr>
-              <th>Sl. No.</th>
               <th>Title</th>
               <th>Date</th>
-              <th>Time</th>
+              <th>Time(UTC+5:30)</th>
               <th>Type</th>
               <th>Host</th>
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <td>1</td>
-              <td>Loren Ipsum sit dolor emit blah blah</td>
-              <td>10/03/2021</td>
-              <td>Loren Ipsum</td>
-              <td> <h6 className="mini-events"> Mini Event </h6> </td>
-              <td>Loren Ipsum</td>
-            </tr>
-            
-            <tr>
-              <td>2</td>
-              <td>Loren Ipsum sit dolor emit blah blah</td>
-              <td>11/03/2021</td>
-              <td>5:30 PM</td>
-              <td> <h6 className="workshops"> Workshop </h6> </td>
-              <td>Loren Ipsum</td>
-            </tr>
-
-            <tr>
-              <td>1</td>
-              <td>Nikita Gandhi's Talk</td>
-              <td>12/03/2021</td>
-              <td>5:30 PM</td>
-              <td> <h6 className="important-time-markers"> Time Markers </h6> </td>
-              <td>Loren Ipsum</td>
-            </tr>
-            
-            <tr>
-              <td>2</td>
-              <td>Wikipedia Run</td>
-              <td>13/03/2021</td>
-              <td>10:00 PM</td>
-              <td> <h6 className="sessions"> Session </h6> </td>
-              <td>Loren Ipsum</td>
-            </tr>
-
-            <tr>
-              <td>1</td>
-              <td>Siddharth's Workshop</td>
-              <td>14/03/2021</td>
-              <td>Loren Ipsum</td>
-              <td> <h6 className="workshops"> Workshop </h6> </td>
-              <td>Loren Ipsum</td>
-            </tr>
-            
-            <tr>
-              <td>2</td>
-              <td>Microsoft Learn Student Ambassador's Talk</td>
-              <td>15/03/2021</td>
-              <td>Loren Ipsum</td>
-              <td> <h6 className="sessions"> Session </h6> </td>
-              <td>Loren Ipsum</td>
-            </tr>
-
-            <tr>
-              <td>1</td>
-              <td>Headstart to Competitive Programming - CodeChef</td>
-              <td>16/03/2021</td>
-              <td>8:00 PM</td>
-              <td> <h6 className="workshops"> Workshop </h6> </td>
-              <td>Loren Ipsum</td>
-            </tr>
-            
-            <tr>
-              <td>2</td>
-              <td>Kahoot</td>
-              <td>17/03/2021</td>
-              <td>10:00 PM</td>
-              <td> <h6 className="workshops"> Workshop </h6> </td>
-              <td>Loren Ipsum</td>
-            </tr>
-
-            <tr>
-              <td>1</td>
-              <td>AWS for Developers</td>
-              <td>18/03/2021</td>
-              <td>Loren Ipsum</td>
-              <td> <h6 className="workshops"> Workshop </h6> </td>
-              <td>Loren Ipsum</td>
-            </tr>
-
-            <tr>
-              <td>1</td>
-              <td>Mentor Booth</td>
-              <td>18/03/2021</td>
-              <td>Loren Ipsum</td>
-              <td> <h6 className="mini-events"> Mini Event </h6> </td>
-              <td>Loren Ipsum</td>
-            </tr>
-
-            <tr>
-              <td>1</td>
-              <td>Team Building Events: Among Us, Psych, skribble, Codenames</td>
-              <td>18/03/2021</td>
-              <td>10:00 PM</td>
-              <td> <h6 className="mini-events"> Mini Event </h6> </td>
-              <td>Loren Ipsum</td>
-            </tr>
-            
-            <tr>
-              <td>2</td>
-              <td>Keynote Session</td>
-              <td>19/03/2021</td>
-              <td>5:00 PM</td>
-              <td> <h6 className="sessions"> Session </h6> </td>
-              <td>Loren Ipsum</td>
-            </tr>
-
-            <tr>
-              <td>2</td>
-              <td>Gaurav Sen Session</td>
-              <td>19/03/2021</td>
-              <td>6:00 PM</td>
-              <td> <h6 className="sessions"> Session </h6> </td>
-              <td>Loren Ipsum</td>
-            </tr>
-
-            <tr>
-              <td>2</td>
-              <td>Check-In</td>
-              <td>19/03/2021</td>
-              <td>7:30 PM</td>
-              <td> <h6 className="workshops"> Workshop </h6> </td>
-              <td>Loren Ipsum</td>
-            </tr>
-
-            <tr>
-              <td>2</td>
-              <td>Hacking Hours Begin</td>
-              <td>19/03/2021</td>
-              <td>10:00 PM</td>
-              <td> <h6 className="workshops"> Workshop </h6> </td>
-              <td>Loren Ipsum</td>
-            </tr>
-
-            <tr>
-              <td>2</td>
-              <td>Mini Event</td>
-              <td>19/03/2021</td>
-              <td>10:30 PM</td>
-              <td> <h6 className="mini-events"> Mini Event </h6> </td>
-              <td>Loren Ipsum</td>
-            </tr>
-
-            <tr>
-              <td>1</td>
-              <td>Mentor Booth</td>
-              <td>20/03/2021</td>
-              <td>Loren Ipsum</td>
-              <td> <h6 className="mini-events"> Mini Event </h6> </td>
-              <td>Loren Ipsum</td>
-            </tr>
-
-            <tr>
-              <td>1</td>
-              <td>Mini Event</td>
-              <td>20/03/2021</td>
-              <td>Loren Ipsum</td>
-              <td> <h6 className="mini-events"> Mini Event </h6> </td>
-              <td>Loren Ipsum</td>
-            </tr>
-
-            <tr>
-              <td>1</td>
-              <td>CodeChef Hour</td>
-              <td>20/03/2021</td>
-              <td>2:00 PM</td>
-              <td> <h6 className="workshops"> Workshop </h6> </td>
-              <td>Loren Ipsum</td>
-            </tr>
-
-            <tr>
-              <td>1</td>
-              <td>Logan Kilpatrick's Session</td>
-              <td>20/03/2021</td>
-              <td>7:30 PM</td>
-              <td> <h6 className="sessions"> Session </h6> </td>
-              <td>Loren Ipsum</td>
-            </tr>
-
-            <tr>
-              <td>1</td>
-              <td>Mini Event</td>
-              <td>20/03/2021</td>
-              <td>Loren Ipsum</td>
-              <td> <h6 className="mini-events"> Mini Event </h6> </td>
-              <td>Loren Ipsum</td>
-            </tr>
-            
-            <tr>
-              <td>2</td>
-              <td>Hacking Hours End</td>
-              <td>21/03/2021</td>
-              <td>10:00 AM</td>
-              <td> <h6 className="workshops"> Workshop </h6> </td>
-              <td>Loren Ipsum</td>
-            </tr>
-
-            <tr>
-              <td>2</td>
-              <td>DSC Manager Session</td>
-              <td>21/03/2021</td>
-              <td>6:00 PM</td>
-              <td> <h6 className="sessions"> Session </h6> </td>
-              <td>Loren Ipsum</td>
-            </tr>
-
-            <tr>
-              <td>2</td>
-              <td>Project Showcase</td>
-              <td>21/03/2021</td>
-              <td>Loren Ipsum</td>
-              <td> <h6 className="workshops"> Workshop </h6> </td>
-              <td>Loren Ipsum</td>
-            </tr>
-
-            <tr>
-              <td>2</td>
-              <td>Juan Pablo's Session</td>
-              <td>21/03/2021</td>
-              <td>10:00 PM</td>
-              <td> <h6 className="workshops"> Workshop </h6> </td>
-              <td>Loren Ipsum</td>
-            </tr>
-
-            <tr>
-              <td>2</td>
-              <td>Prizes Announcement</td>
-              <td>27/03/2021</td>
-              <td>lorem</td>
-              <td> <h6 className="workshops"> Workshop </h6> </td>
-              <td>Loren Ipsum</td>
-            </tr>
+            {
+              timelineData.map((date,i) => {
+                return date.events.map((event) => (
+                    <tr>
+                    <td style={{alignContent:"left"}}>{event.title}</td>
+                    <td>{date.date}</td>
+                    <td>{event.time}</td>
+                    <td> <h6 className={event.type.trim().toLowerCase()}> {event.type} </h6> </td>
+                    <td>{event.host}</td>
+                  </tr>
+                ))
+              } )
+            }
           </tbody>
         </table>
       </div>
